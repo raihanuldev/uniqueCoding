34bb7ceca63a2512ccd347cc87fb19319b389944
Rihanul Islam <rihanthedev@gmail.com>
2022-12-06 02:51:18 +0600

    ps

---
+++ problemSolveing01.py
@@ -0,0 +1,14 @@
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+save = str(savings)
+ans = str(result)
+print("I started with $" + save + " and now have $" + ans + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
