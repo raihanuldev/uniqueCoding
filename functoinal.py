a9281a971f0704bd81ebbe1f7b6ef33e3bce42bc
Rihanul Islam <rihanthedev@gmail.com>
2023-02-27 22:59:58 +0600

    def practies

---
+++ functoinal.py
@@ -0,0 +1,8 @@
+def bio():
+    print('Hello world iam mela din por programmig korte aschi.')
+    nam = 'skolani ';
+    age = 15;
+    print('Your Name Is :', nam);
+    print('Your Age IS:', age);
+
+bio()
