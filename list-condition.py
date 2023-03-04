a64e9db93abbd9b3ed44a672f3ac6d780b246554
Rihanul Islam <rihanthedev@gmail.com>
2023-03-04 13:43:11 +0600

    list items conditon

---
+++ list-condition.py
@@ -0,0 +1,9 @@
+name = ["Pospo","Asfi","Riya","Marufa","Jonnatul Boika"];
+if "Pospoq" in name:
+    print('She Is Your Frist Ex');
+elif "Riyza" in name:
+    print("She Is Your Secound Ex");
+elif 'maruda' in name:
+    print('Wrong')
+else:
+    print('Okey Check All Information ,done')
