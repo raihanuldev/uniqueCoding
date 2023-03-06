f6035127d3bce287df6a81da3f26af31665978f6
Rihanul Islam <rihanthedev@gmail.com>
2023-03-06 12:23:38 +0600

    conditional

---
+++ condition/IsAgeIsLessThan18.py
@@ -0,0 +1,9 @@
+userName = input("What Is Your Name: ")
+userAge = input("What Is Your Age: ")
+if userAge > '18':
+    print('Your Name Is: ', userName)
+    print('Your Age Is ', userAge)
+    print('You are now on our condition')
+
+else:
+    print('dur beDA Toi amder conditon e poros nah ')
