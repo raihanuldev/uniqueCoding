c3544866652879c7816be0976b728d250db6d733
Rihanul Islam <rihanthedev@gmail.com>
2023-03-25 03:48:51 +0600

    resume

---
+++ Problem solveing/05.py
@@ -0,0 +1,7 @@
+π = 3.14159;
+R = input('');
+R = float(R)
+A = π*R*2;
+A=(round(A,4));
+A=str(A)
+print('A='+A)
---
+++ simple-10-Projects-idea/Resturent ideas/Order List/main.py
@@ -0,0 +1,11 @@
+# # Noted: Just Try do something about python. FeedBack Is Always accepte as my Learning Part.
+# print("Welcome To Jhaka Naka Resturent.")
+# print("We have Available 5 Items for Sell.   ")
+# print("1: Kutta Pray for Sultan dine' ")
+# print("2: Snake Pary for chines babu' ")
+# print("3: Mew pray for babunir ammor' ")
+# print("4: Koliza pray for Snake' ")
+# print("5: bhai aro noton kichu jante chaw naki? ")
+# menuList = input('Type Serial Number For details: ')
+# if menuList =="1":
+#     print('You Selected Your ')
