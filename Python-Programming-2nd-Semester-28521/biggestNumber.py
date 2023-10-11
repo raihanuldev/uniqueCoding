a7ee4c9f242736d23b62f2d530d84c24c90d4337
uniquecoderrihan <rihanthedev@gmail.com>
2023-10-11 21:21:28 +0600

    28521

---
+++ Python-Programming-2nd-Semester-28521/biggestNumber.py
@@ -0,0 +1,18 @@
+# At Frist Take input from User::
+a = int(input("Enter A Value: "))
+b = int(input("Enter B Value: "))
+c = int(input("Enter C Value: "))
+
+# Okey We have 3 Number . 
+# now we will Check Which is Biggest Number.
+
+if a > b and a>c:
+    print("A Is Biggest Number ", a)
+elif b > c and b>a:
+    print("B Is Biggest Number ", b)
+else:
+    print("C Is Biggest Number ", c)
+
+# now we run our program
+# Thank You Everyone... 
+# i will Provide code on Video description.
